484b6a2626c0017ee143a24130779ad62e3c4b77
asivitskiy <sivikmail@gmail.com>
2021-02-17 22:54:40 +0700

    rename pol++++

--- poliglotiki/mailsender.php
+++ poliglotiki/mailsender.php
@@ -1,10 +1,10 @@
 <?php
 $to = 'sivikmail@gmail.com';
-$subject = '=?utf-8?B?'.base64_encode('Новое сообщение!').'?=';
-$headers = 'From: =?UTF-8?B?' . base64_encode($name) . '?= <=?UTF-8?B?' . base64_encode($name) . "?=>\r\n";
+$subject = '=?utf-8?B?'.base64_encode('Тут тема').'?=';
+$headers .= "From: От кого письмо <from@example.com>\r\n";
 $headers .= 'Return-path: <' . $email . ">\r\n";
 $headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n";
 $headers .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
-$message = "Новый запрос!\n\nИмя: $name\n\nEmail: $email\n\nТелефон: $tel\n\n";
+$message = "Тут сообщение";
 $mail = mail($to, $subject, $message, $headers);
 ?>
