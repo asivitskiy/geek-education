8aa0fa0a28f73aef35b28ed3010afb9a9dbb9e17
asivitskiy <sivikmail@gmail.com>
2021-02-17 22:57:22 +0700

    rename pol+++++

--- poliglotiki/mailsender.php
+++ poliglotiki/mailsender.php
@@ -1,5 +1,5 @@
 <?php
-$to = 'sivikmail@gmail.com';
+$to = 'test-ktcf5uyk8@srv1.mail-tester.com';
 $subject = '=?utf-8?B?'.base64_encode('Тут тема').'?=';
 $headers .= "From: От кого письмо <from@example.com>\r\n";
 $headers .= 'Return-path: <' . $email . ">\r\n";
